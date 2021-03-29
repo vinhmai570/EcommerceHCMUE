c30f14ef7ca51ff5518db2e2c478130e5855aba3
Mai Trong Vinh <vinhmai570@gmail.com>
2021-03-29 20:07:05 +0700

    Add admin categories routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+
 Route::get('/dashboard', function () {
     return view('layouts/admin');
+});
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::prefix('categories')->name('categories.')->group(function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('index');
+        Route::get('/create', [CategoryController::class, 'create'])->name('create');
+        Route::post('/create', [CategoryController::class, 'store'])->name('store');
+    });
 });
