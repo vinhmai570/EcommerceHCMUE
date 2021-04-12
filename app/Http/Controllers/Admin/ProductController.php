ef237398a806cfcaf23500703d1ce1cc7ffd0f81
Vinh Mai <vinhmai570@gmail.com>
2021-04-12 22:25:18 +0700

    Done layout create

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Attribute;
+use App\Models\AttributeValue;
 
 class ProductController extends Controller
 {
     public function create()
     {
-        return view('admin.product.create');
+        $categories = Category::all();
+        $attributes = Attribute::all();
+        return view('admin.product.create', compact('categories', 'attributes'));
     }
 }
--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -22,50 +22,64 @@
             <div class="form-body">
               <div class="form-group">
                 <label>Name</label>
-                <input type="text" class="form-control" name="product[name]" placeholder="">
+                <input type="text" class="form-control" name="name" placeholder="">
               </div>
               <div class="form-group">
                 <label>Description</label>
-                <textarea class="form-control" name="product[description]"></textarea>
+                <textarea class="form-control" name="description"></textarea>
               </div>
               <div class="form-group">
                 <label>Content</label>
-                <textarea class="form-control" name="product[short_description]"></textarea>
+                <textarea class="form-control" name="short_description"></textarea>
               </div>
               <div class="form-group">
                 <label>Image</label>
                 <input type="file" class="form-control" name="image">
               </div>
               <div class="row">
-                <div class="col-md-3 col-sm-12">
+                @foreach ($attributes as $attribute)
+                <div class="col-md-2 col-sm-12">
+                  <div style="margin: 10px">
+                    <div class="form-group">
+                      <label>{{ $attribute->name }}</label>
+                      <select class="table-group-action-input form-control input-medium" name="attribute[]">
+                        @foreach ($attribute->attribute_values as $value)
+                          <option value="{{ $value->id }}">{{ $value->value_name }}</option>                  
+                        @endforeach
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                @endforeach
+                <div class="col-md-2 col-sm-12">
                   <div style="margin: 10px">
                     <div class="form-group">
                       <label>Sku</label>
-                      <input type="text" class="form-control" name="product[name]" placeholder="">
+                      <input type="text" class="form-control" name="sku" placeholder="">
                     </div>
                   </div>
                 </div>
-                <div class="col-md-3 col-sm-12">
+                <div class="col-md-2 col-sm-12">
                   <div style="margin: 10px">
                     <div class="form-group">
                       <label>Price</label>
-                      <input type="number" class="form-control" name="product[name]" placeholder="">
+                      <input type="number" class="form-control" name="price" placeholder="">
                     </div>
                   </div>
                 </div>
-                <div class="col-md-3 col-sm-12">
+                <div class="col-md-2 col-sm-12">
                   <div style="margin: 10px">
                     <div class="form-group">
                       <label>Price sale</label>
-                      <input type="number" class="form-control" name="product[name]" placeholder="">
+                      <input type="number" class="form-control" name="price_sale" placeholder="">
                     </div>
                   </div>
                 </div>
-                <div class="col-md-3 col-sm-12">
+                <div class="col-md-2 col-sm-12">
                   <div style="margin: 10px">
                     <div class="form-group">
                       <label>Quantity</label>
-                      <input type="number" class="form-control" name="product[name]" placeholder="">
+                      <input type="number" class="form-control" name="quantity" placeholder="">
                     </div>
                   </div>
                 </div>
@@ -78,8 +92,10 @@
         <div class="portlet light">
           <div class="form-group">
             <label>Category</label>
-            <select class="table-group-action-input form-control input-medium" name="product[tax_class]">
-              <option value="">Select...</option>
+            <select class="table-group-action-input form-control input-medium" name="category">
+            @foreach ($categories as $category)
+              <option value="{{ $category->id }}">{{ $category->name }}</option>                  
+            @endforeach
             </select>
           </div>
         </div>
@@ -87,14 +103,14 @@
           <div class="form-group">
             <label class="control-label" style="margin:0">Is published?</label>
             <hr>
-            <input type="checkbox" checked class="make-switch" data-size="small">
+            <input type="checkbox" checked class="make-switch" data-size="small" name="is_published">
           </div>
         </div>
         <div class="portlet light">
           <div class="form-group">
             <label class="control-label" style="margin:0">Is featured?</label>
             <hr>
-            <input type="checkbox" class="make-switch" data-size="small">
+            <input type="checkbox" class="make-switch" data-size="small" name="is_featured">
           </div>
         </div>
       </div>
