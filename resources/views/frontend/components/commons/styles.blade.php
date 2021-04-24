732350538b9acb3739e090c813bbbbbf7e843181
Mai Trong Vinh <vinhmai570@gmail.com>
2021-04-24 21:29:57 +0700

    Split components

---
+++ resources/views/frontend/components/commons/styles.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/bootstrap.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/style.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/vendor/owl-slider.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/vendor/settings.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/vendor/animate.css') }}"/>
---
+++ resources/views/frontend/components/content.blade.php
@@ -0,0 +1,6 @@
+<div class="main-content">
+    @include('frontend/components/featured_product')
+    @include('frontend/components/popular_product')
+    @include('frontend/components/banner')
+    @include('frontend/components/best_seller_product')
+</div>
