19a2ddb9fc77bb5aab7d8aa016c3263320b78b62
VanThuong <vanthuonghcmue@gmail.com>
2021-05-04 19:24:01 +0700

    add edit profile user

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'phone' => 'min:10|max:11',
+            'birthday'=> 'date',
+            'address' => 'string',
+            'image' => 'image|mimes:jpg,png,jpeg,gif,svg'
+        ];
+    }
+}
