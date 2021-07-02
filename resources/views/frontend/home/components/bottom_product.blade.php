7f8b145d6f474d43c073885ddf31fd77f90bb8c3
Mai Trong Vinh <vinhmai570@gmail.com>
2021-07-03 00:14:21 +0700

    fix: show bottom products item0

--- resources/views/frontend/home/components/bottom_product.blade.php
+++ resources/views/frontend/home/components/bottom_product.blade.php
@@ -4,21 +4,21 @@
             <div class="title-text size-25">
                 <h3><span>F</span>eatured product</h3>
             </div>
-            @each('frontend.home.components.product_item', $bottom_products_featured, 'product')
+            @each('frontend.home.components.bottom_product_item', $bottom_products_featured, 'product')
         </div>
         <!-- End col-md-4 -->
         <div class="col-md-4">
             <div class="title-text size-25">
                 <h3><span>B</span>est Seller</h3>
             </div>
-            @each('frontend.home.components.product_item', $bottom_products_best_seller, 'product')
+            @each('frontend.home.components.bottom_product_item', $bottom_products_best_seller, 'product')
         </div>
         <!-- End col-md-4 -->
         <div class="col-md-4">
             <div class="title-text size-25">
                 <h3><span>N</span>ewest Products</h3>
             </div>
-            @each('frontend.home.components.product_item', $bottom_products_newest, 'product')
+            @each('frontend.home.components.bottom_product_item', $bottom_products_newest, 'product')
         </div>
         <!-- End col-md-4 -->
     </div>
