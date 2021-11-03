d732beb795473d9741fee7b0f0eb4e4df3b7e285
Mai Trong Vinh <vinhmai570@gmail.com>
2021-11-03 16:57:30 +0700

    add google-site-verification

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="{{ env('GOOGLE_SITE_VERIFICATION') }}" />
     <link rel="shortcut icon" href="{{asset('frontend/images/favicon.png')}}" />
     @include('frontend.components.styles')
     <title>Home</title>
